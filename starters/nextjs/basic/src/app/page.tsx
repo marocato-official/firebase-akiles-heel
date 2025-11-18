6f61f788a2374ffe7d6123e0c97aae74ea8855a4
Maro Akile <marocato.official@gmail.com>
2025-11-18 23:30:40 +0800

    Update page.tsx

--- starters/nextjs/basic/src/app/page.tsx
+++ starters/nextjs/basic/src/app/page.tsx
@@ -6,61 +6,37 @@ export default function Home() {
     <main className="content">
       <div id="posts">
         <div>
-          <h1> NEWS </h1>
+          <h1> {"<"} NEWS {">"} </h1>
           <img src="https://i.imgur.com/AeDlNOd.png" width="100%"/>
           <br/><br/>
           <h2> Project Prototype Release: A2M (Audio-to-MIDI) </h2>
           <p> Audio File to MIDI tool. Intended for single-track recordings. Currently available online and as a downloadable .exe file. </p>
-          <p> Use the online version{" "}<Link href="/A2M">here</Link>{" "}! (Redirects to a different page on this website.)</p>
+          <p> Use the online version{" "}<Link href="/A2M">here</Link>! (Redirects to a different page on this website.)</p>
         </div>
-      </div>
-      <h1 className="heading">Next.js on Firebase App Hosting</h1>
-      <h2> hi its akile </h2>
-      <p>{message}</p>
 
-      <section className="features">
-        <article className="card">
-          <h2>Scalable, serverless backends</h2>
-          <p>
-            Dynamic content is served by{" "}
-            <Link
-              href="https://cloud.google.com/run/docs/overview/what-is-cloud-run"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Cloud Run
-            </Link>
-            , a fully managed container that scales up and down with demand.
-            Visit{" "}
-            <Link href="/ssr">
-              <code>/ssr</code>
-            </Link>{" "}
-            and{" "}
-            <Link href="/ssr/streaming">
-              <code>/ssr/streaming</code>
-            </Link>{" "}
-            to see the server in action.
-          </p>
-        </article>
-        <article className="card">
-          <h2>Global CDN</h2>
-          <p>
-            Cached content is served by{" "}
-            <Link
-              href="https://cloud.google.com/cdn/docs/overview"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Google Cloud CDN
-            </Link>
-            , a fast and secure way to host cached content globally. Visit
-            <Link href="/ssg">
-              {" "}
-              <code>/ssg</code>
-            </Link>{" "}
-          </p>
-        </article>
-      </section>
+        <div>
+          <h2> &lt; Projects &gt; </h2>
+          
+          <div class="tab">
+  					<h3> Lunti CVVC </h3>
+  					<p> <b>STATUS:</b> IN DEVELOPMENT </p>
+  					<p> An English CVVC X-SAMPA UTAU voicebank. </p>
+				  </div>
+
+          <div class="tab">
+  					<h3> Quill for TalonVoice </h3>
+  					<p> <b>STATUS:</b> ABANDONED {" "} <Link href="https://github.com/marocato-official/talonvoice-quill">github</Link> </p>
+  					<p> A module for TalonVoice. Made for a friend. </p>
+				  </div>
+
+          <div class="tab">
+  					<h3> A2M (Audio-to-MIDI) </h3>
+  					<p> <b>STATUS:</b> PROTOTYPE {" "}<Link href="/A2M">online</Link> {" "}<Link href="https://akiles-heel.itch.io/a2m-audio-to-midi-wip">itch</Link> </p>
+  					<p> Audio to MIDI tool. </p>
+				  </div>
+        </div>
+        
+      </div>
     </main>
   );
 }
